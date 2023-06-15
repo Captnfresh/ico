bd7564a31ad4bb4216bae0111514e82b9fb29dad
Captn fresh <captainfresh.aa1@gmail.com>
2023-06-15 12:58:15 +0100

    second commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xA7Bb74c89Aa33eF7Ef339E99D2DfF51E3863c69A";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
